3a1d9cb1511d5e32886455f659a2e2e87d707c77
Ilya Trokhin <vertvvv@gmail.com>
2018-11-12 17:20:20 +0300

    Add empty react-redux boilerplate

---
+++ src/components/ProductRow.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const ProductRow = ({ data }) =>
+  <div>
+    <p>
+      {data.name}
+      {' '}
+      =
+      {' '}
+      {data.price}
+      {' '}
+    </p>
+  </div>;
+
+ProductRow.propTypes = {
+  data: PropTypes.object,
+};
+
+export default ProductRow;
