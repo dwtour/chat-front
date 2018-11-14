8c8a00dd93c4b221ed88449abf48288648399c2a
Kholod Darya <delichka36@gmail.com>
2018-11-14 23:06:49 +0300

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,70 @@
-import React from 'react'
-import {render} from 'react-dom'
-import Layout from "./components/Layout";
-import Input from "./components/Input";
+import React, { Component} from 'react';
 
-render(Layout(), document.getElementById("messages-container"));
-render(Input(), document.getElementById('message-input'));
+import Input from './Input';
+import Button from './Button';
+import Message from './Message';
+
+import '../styles/index.scss';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [
+        {
+          type: 'message-from',
+          text: '#user1 join',
+        },
+        {
+          type: 'message-from',
+          text: '#user2 leave',
+        },
+        {
+          type: 'message-to',
+          text: 'You: hello hello hello hello hello hello hello',
+        },
+        {
+          type: 'message-from',
+          text: '#user3: hello there',
+        },
+      ],
+    };
+  }
+
+  submitMessage() {
+    const input = document.getElementById('input');
+    if (input.value.length === 0) {
+      alert('Попытка отправить пустое сообщение');
+    } else {
+      const joined = this.state.messages.concat({ type: 'message-to', text: 'You: ' + input.value });
+      this.setState({ messages: joined });
+      input.value = '';
+    }
+  }
+
+  render() {
+    return (
+            <div className="chat">
+                <div className="chat-box" id="messages-container">
+                    <div className="message">
+                        {
+                            this.state.messages.map(message => <Message
+                                                                        type={message.type}
+                                                                        text={message.text}
+                            />)
+                        }
+                    </div>
+                <div className="client-wrapper">
+                    <div className="input-wrapper">
+                        <Input id="input" />
+                        <Button submitMessage={this.submitMessage.bind(this)} />
+                    </div>
+                    <p className="ip">you joined as 127:0:0:1</p>
+                </div>
+                </div>
+            </div>
+    );
+  }
+}
+
+export default App;
