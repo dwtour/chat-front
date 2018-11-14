42d35e42ba77f6afda5abf0b1c38600dec7712ba
Kholod Darya <delichka36@gmail.com>
2018-11-14 23:08:33 +0300

    Delete MessageFrom.js

--- src/components/MessageFrom.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import Text from 'Text'
-
-const MessageFrom = () =>
-        <div className="chat-layout__messages__message-from">
-            <Text />
-        </div>
-
-export default MessageFrom;
