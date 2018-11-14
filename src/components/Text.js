84571ccaf0f602c262fd1c72bad7abeb43814aeb
Kholod Darya <delichka36@gmail.com>
2018-11-14 23:09:05 +0300

    Delete Text.js

--- src/components/Text.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-const message = ['#user1 join', '#user2 leave', 'You: hello hello hello hello hello hello hello', '#user3: hello there'];
-
-const Text = () =>
-        <p className="text">{message[1]}</p>
-
-export default Text;
